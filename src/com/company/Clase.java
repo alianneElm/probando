07811085f47c986f8894a6eb6c90be8dac193ae2
Alianne <alianneelm@yahoo.se>
2020-10-06 12:46:46 +0200

    corregidos errores creacion de nuevo metodo

--- src/com/company/Clase.java
+++ src/com/company/Clase.java
@@ -4,18 +4,25 @@ public class Clase {
 
     String clase1;
     int clase2;
+    int clase3;
 
     public void Clase(){
         this.clase1 = clase1;
         this.clase2 = clase2;
+        this.clase3= clase3;
+
     }
 
     public void claseToString(){
 
-        String final = "La clase 1 y 2 no 
-        se pueden sumar porque tienen variables diferentes";
+        String terminado = "La clase 1 y 2 no se pueden sumar porque tienen variables diferentes";
 
-        System.out.println("Ahora si que si: "+ final);
-        
+        System.out.println("Ahora si que si: "+ terminado);
+       // habia que hacer unos cambios porque no funcionaba el codigo
     }
+    public void sumaClases(){
+        int laSuma = clase2 + clase3;
+        System.out.println("La suma de las dos clases es "+ laSuma);
+    }
+
 }
