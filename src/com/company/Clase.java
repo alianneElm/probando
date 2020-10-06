6322aef288d6bdf6903fd0bfec8a12121f217eac
Alianne <alianneelm@yahoo.se>
2020-10-06 12:06:47 +0200

    Includerad Konstruktor i Klassen

--- src/com/company/Clase.java
+++ src/com/company/Clase.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Clase {
+
+    String clase1;
+    int clase2;
+
+    public void Clase(){
+        this.clase1 = clase1;
+        this.clase2 = clase2;
+    }
 }
